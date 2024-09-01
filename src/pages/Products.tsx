fce9df83f7aac1d430ea97e4c993054158909ed2
Bimsara Walallawita <bwalallawita@gmail.com>
2024-09-01 09:57:01 +0530

    Product CRUD

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -56,6 +56,35 @@ function Products() {
         loadCategories();
     }, [])
 
+    const [productEditing, setProductEditing] = useState<ProductType | null>();
+
+    function editProduct(product: ProductType) {
+        setProductEditing(product);
+        setProductName(product.name);
+        setProductPrice(product.price);
+        setCategoryId(product.category.id);
+        setDescription(product.description);
+    }
+
+    async function updateProduct() {
+        try {
+            await axios.put(`http://localhost:8081/products/${productEditing?.id}`, {
+                name: productName,
+                price: productPrice,
+                categoryId: categoryId,
+                description: description
+            });
+            getProducts();
+            setProductEditing(null);
+            setProductName("");
+            setProductPrice(0);
+            setCategoryId(0);
+            setDescription("");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="container mx-auto pt-5 pb-5">
             <h1 className="text-3xl font-semibold mb-5 text-slate-800">
@@ -69,6 +98,7 @@ function Products() {
                         <th className="p-2 w-[200px] text-left">Product Name</th>
                         <th className="p-2 text-left w-[100px]">Product Price</th>
                         <th className="p-2 text-left w-[200px]">Category</th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -79,6 +109,10 @@ function Products() {
                                 <td className="p-2 text-slate-600 border-b border-slate-200">{product.name}</td>
                                 <td className="p-2 text-slate-600 text-right border-b border-slate-200">{product.price}</td>
                                 <td className="p-2 text-slate-600 border-b border-slate-200">{product.category.name}</td>
+                                <td className="p-2 border-b border-slate-200">
+                                    <button type="button"
+                                        onClick={() => editProduct(product)}>Edit</button>
+                                </td>
                             </tr>
                         )
                     })}
@@ -86,21 +120,23 @@ function Products() {
             </table>
 
             <div className="mt-10 w-[650px] border border-slate-200 px-4 py-3 rounded-lg">
-                <h2 className="text-xl font-medium mb-4">Add Product</h2>
+                <h2 className="text-xl font-medium mb-4">
+                    {productEditing ? 'Edit Product' : 'Add Product'}
+                </h2>
 
                 <div className="mb-4">
                     <label className="text-sm text-slate-600 block mb-3">Enter product name</label>
-                    <input type="text" className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" onChange={handleProductName} />
+                    <input type="text" className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" value={productName} onChange={handleProductName} />
                 </div>
 
                 <div className="mb-4">
                     <label className="text-sm text-slate-600 block mb-3">Enter product price</label>
-                    <input type="number" className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" onChange={handleProductPrice} />
+                    <input type="number" className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" value={productPrice} onChange={handleProductPrice} />
                 </div>
 
                 <div className="mb-4">
                     <label className="text-sm text-slate-600 block mb-3">Enter category id</label>
-                    <select className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" onChange={handleCategoryId} >
+                    <select className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" value={categoryId} onChange={handleCategoryId} >
                         <option value="">Select Category</option>
 
                         {categories.map(function (category) {
@@ -113,10 +149,15 @@ function Products() {
 
                 <div className="mb-4">
                     <label className="text-sm text-slate-600 block mb-3">Enter description</label>
-                    <textarea className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" onChange={handleDescription}></textarea>
+                    <textarea className="block w-full p-2 border border-slate-300 rounded-lg text-slate-600 text-sm" value={description} onChange={handleDescription}>
+                    </textarea>
                 </div>
 
-                <button className="py-2 px-3 rounded-lg bg-slate-800 text-sm text-white hover:bg-slate-950" onClick={saveProduct}>Add Product</button>
+                {productEditing ? (
+                    <button className="py-2 px-3 rounded-lg bg-slate-800 text-sm text-white hover:bg-slate-950" onClick={updateProduct}>Update Product</button>
+                ) : (
+                    <button className="py-2 px-3 rounded-lg bg-slate-800 text-sm text-white hover:bg-slate-950" onClick={saveProduct}>Add Product</button>
+                )}
             </div>
         </div>
     )
